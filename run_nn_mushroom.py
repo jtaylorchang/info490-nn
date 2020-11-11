0ccf42429d3fb6a3a47145118633c2fe1296e605
Jeff Taylor-Chang <jeff@jefftc.com>
2020-11-10 19:29:25 -0600

    Updated print format

--- run_nn_mushroom.py
+++ run_nn_mushroom.py
@@ -48,9 +48,10 @@ def train(net, epochs, batch_size, learning_rate, learning_rate_decay, regulariz
         # No need to run the backward pass here, just run the forward pass to compute accuracy
         val_accuracy[epoch] += net.forward_accuracy(X_val, y_val)
 
-        print('train_loss:', train_loss[epoch])
-        print('train_accuracy:', train_accuracy[epoch])
-        print('val_accuracy:', val_accuracy[epoch])
+        stats_string = "Loss: {train_loss:.1f}, Train: {train_accuracy:.1f}%, Val: {val_accuracy:.1f}%"
+        print(stats_string.format(train_loss=train_loss[epoch],
+                                  train_accuracy=train_accuracy[epoch] * 100,
+                                  val_accuracy=val_accuracy[epoch] * 100))
 
     return train_loss, train_accuracy, val_accuracy
 
